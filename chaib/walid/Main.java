c77a95aa1d50f0a6c2bdc4c9b74cb79e6c7f69dc
Walid26 <32452287+Walid26@users.noreply.github.com>
2018-05-23 23:40:34 +0200

    Update Main.java

--- chaib/walid/Main.java
+++ chaib/walid/Main.java
@@ -16,25 +16,13 @@ public class Main {
         Player player2 = new Player(playerName);
         Game game = new Game(player1, player2, null);
         game.initializeGame();
-        int hasHit;
         while (!game.isOver()) {
             System.out.println("Get ready " + game.getActivePlayer().getName());
             do {
-                System.out.println("Enter a missile coordinate: ");
+                System.out.println("Enter a missile coordinate : ");
                 missileCoordinates = scan.nextLine();
-                hasHit = game.getActivePlayer().launchMissile(missileCoordinates, game.getPassivePlayer());
-                if (hasHit == 1) {
-                    System.out.println("Your missile touched the opponent ! Continue like this !");
-                } else if (hasHit == 0) {
-                    System.out.println("Your missile plunged in water. Better luck next time !");
-                }
-            } while (hasHit == -1);
-            if (game.newRound())
-                System.out.println(game.toString());
         }
-        System.out.println("Game is over!");
-        System.out.println("Congratulations " + game.getWinner().getName() + ", you win with "
-                + game.getWinner().getNbAvailableShips() + " ships left!");
+        System.out.println("Winner : " + game.getWinner().getName());
         scan.close();
     }
 }
